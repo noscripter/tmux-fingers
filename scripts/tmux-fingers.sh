9bc4d832102a6085bc6d4c6620b06792fffe295e
Jorge Morante <jorge@morante.eu>
2016-05-14 19:43:22 +0200

    perf traces

--- scripts/tmux-fingers.sh
+++ scripts/tmux-fingers.sh
@@ -3,6 +3,8 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $CURRENT_DIR/utils.sh
 
+log "init $(current_ms)"
+
 function init_fingers_pane() {
   local pane_id=$(tmux new-window -F "#{pane_id}" -P -d -n "!fingers")
   echo $pane_id
